6bdf88b4a08a748ff573f971b8c865370b890fd0
marcelmukundi <marcel.mukundi@wsu.edu>
2022-02-19 19:05:52 -0800

    set up waitress server

--- PythonBackend/main.py
+++ PythonBackend/main.py
@@ -18,3 +18,8 @@ def post_post():
     return jsonify({
         "response": f"Hi {name}!"
     })
+
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
