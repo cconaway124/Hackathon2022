997f68f2299a55159948189dcecd0dce002f4169
marcelmukundi <marcel.mukundi@wsu.edu>
2022-02-19 22:23:06 -0800

    Initial hooks for connecting to db from react-side

--- frontend/src/Constants.ts
+++ frontend/src/Constants.ts
@@ -2,5 +2,7 @@ import { User } from "./Types/Post"
 
 export const AppName: string = "Looking4"
 
-export const TestPoster: User = { name: "Poster", email: "post@email.email", roles: [] }
-export const TestResponder: User = { name: "Responder", email: "respond@email.email", roles: [] }
+export const APIUrl: string = "https://wsu-hackathon-2022-server-ma5du.ondigitalocean.app"
+
+export const TestPoster: User = { userId: 4, name: "Poster", email: "post@email.email", roles: [] }
+export const TestResponder: User = { userId: 3, name: "Responder", email: "respond@email.email", roles: [] }
