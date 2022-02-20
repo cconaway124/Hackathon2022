79685d08601991162ee8e750e4ec3be5fa5783e4
marcelmukundi <marcel.mukundi@wsu.edu>
2022-02-19 18:26:53 -0800

    Implemented simple web server

--- PythonBackend/main.py
+++ PythonBackend/main.py
@@ -1,8 +1,20 @@
-from flask import Flask
+from flask import request, jsonify, Flask
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
+@app.get("/post")
+def get_posts():
+    print("These are posts")
+    print(request)
     return "<p>Hello, World!</p>"
+
+
+@app.post("/post")
+def post_post():
+    print("Post post")
+    requestJson = request.get_json()
+    name = requestJson["name"]
+    return jsonify({
+        "response": f"Hi {name}!"
+    })
